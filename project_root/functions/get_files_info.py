bf741a50be7e94ed4a2e7833d872e37a5fd33041
Omnisslayer01 <abduldiyandeshmukh2005@gmail.com>
2026-05-20 18:44:58 +0000

    Better formatting of the text to be sent to AI

--- project_root/functions/get_files_info.py
+++ project_root/functions/get_files_info.py
@@ -5,30 +5,28 @@ def get_files_info(working_directory: str, directory: str=".") -> str:
         working_dir_abs=os.path.abspath(working_directory)
         target_dir=os.path.normpath(os.path.join(working_dir_abs,directory))
         valid_target_dir=os.path.commonpath([working_dir_abs,target_dir])==working_dir_abs
+
+        
+        if directory==".":
+            directory="current"
+        final_str=f"Result for {directory} directory: \n"
+
         if not valid_target_dir:
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-        print(target_dir)
+            return f'{final_str} \t Error: Cannot list "{directory}" as it is outside the permitted working directory'
         valid_directory=os.path.isdir(target_dir)
         
         if not valid_directory:
-            return f'Error: "{directory}" is not a directory'
+            return f'{final_str} \t Error: "{directory}" is not a directory'
 
         files=os.listdir(target_dir)
-        print(files)
-        file_size_list=[]
-        is_file_dir=[]
+        
+        
         for file in files:
             file_path=(os.path.join(target_dir,file))
-            file_size_list.append(os.path.getsize(file_path))
-            is_file_dir.append(os.path.isdir(file_path))
-
-
-        print(file_size_list)
-        print(is_file_dir)
-
+            final_str+=f"\t - {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}\n"
         
-
-
+        return final_str
+            
             
     except Exception as e:
         return f"Error: {e}"
